5f8760429b7391cec94ebc9ba2dd32ff611d1eff
Jesús Campos Márquez <jesus.campos302@alu.uhu.es>
2023-10-14 14:22:16 +0200

    first commit

---
+++ RRHH/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\SuperAdminController;
+use App\Http\Controllers\UserController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+
+    return view('welcome');
+
+});
+
+Route::get('/home', function () {
+
+    return view('home');
+
+});
+
+
+Auth::routes();
+
+Route::get('/admin', [AdminController::class, 'index'])->name('admin');
+Route::get('/superadmin', [SuperAdminController::class, 'index'])->name('superadmin');
+
